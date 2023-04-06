ef759c4db687cb422c572c7fdc144aa7370c9f5e
majdiShomali <majdi.shomali.1997@gmail.com>
2023-04-06 02:59:58 +0200

    add regex

--- app.js
+++ app.js
@@ -19,14 +19,12 @@ event.preventDefault();
   userEmail=event.target.useremail.value;
   userNumber=event.target.usernumber.value;
  
-  if( validateName() &&  validatePassword() && validateEmail() && validateNumber()){
+  if(validateName() &&  validatePassword() && validateEmail() && validateNumber()){
 		arrData.push(userName);
 		sessionStorage.setItem('arrData',JSON.stringify(arrData) );
 		// DataCollected.reset(); 
   }
 
- 
-  
 });
 
 function validateName() {
@@ -59,41 +57,69 @@ return true;
 		return true;
 	}   
 
+
 	function validateEmail(){
-		let iChars = "!#$%^&*()+=-[]\\\';,/{}|\":<>?";
-		let Email = userEmail.toLowerCase();
-		 if(!Email.includes("@")  || !Email.includes(".com") ){
-			alert("Please enter a valid Email EX.. majdi@gamil.com");
-			return false;
-		}
+		if (!/\S+@\S+\.\S+/.test(userEmail)) // search for at least non-whitespace characters
+ {
+	alert("E-mail must be in a valid format such as example@gmail.com");
+	return false;
+}
+	return true;
+
+	}
+
+
+	  // Validate phone number to be " 10 digits starts with 07 "
+	  function validateNumber(){
+	  if (!/^07[0-9]{8}$/.test(userNumber))
+	  {
+		 alert("Phone number must be 10 digits starting with 07");
+		 return false;
+	 }
+return true;
+	}
+
+
+
+	// function validateEmail(){
+	// 	let iChars = "!#$%^&*()+=-[]\\\';,/{}|\":<>?";
+	// 	let Email = userEmail.toLowerCase();
+	// 	 if(!Email.includes("@")  || !Email.includes(".com") ){
+	// 		alert("Please enter a valid Email EX.. majdi@gamil.com");
+	// 		return false;
+	// 	}
 
 		
-		for (let i = 0; i < Email.length; i++) {
-			if (iChars.indexOf(Email.charAt(i)) != -1) {
-			alert ("The Email has special characters. \nThese are not allowed.\n");
-			return false;
-				}
-			}
-			return true;
-		}   
+	// 	for (let i = 0; i < Email.length; i++) {
+	// 		if (iChars.indexOf(Email.charAt(i)) != -1) {
+	// 		alert ("The Email has special characters. \nThese are not allowed.\n");
+	// 		return false;
+	// 			}
+	// 		}
+	// 		return true;
+	// 	}   
 
-		function validateNumber(){
-			let iChars = "!#$%^&*()+=-[]\\\';,/{}|\":<>?@.";
-			let Number = userNumber;
+
+
+
+
+		// function validateNumber(){
+		// 	let iChars = "!#$%^&*()+=-[]\\\';,/{}|\":<>?@.";
+		// 	let Number = userNumber;
   
-			 if( !(Number.includes("07"))    || !(Number.length ==10)  ){
-				alert("Please enter a valid number ex. 0799855850");
-				return false;
-			}
+		// 	 if( !(Number.includes("07"))    || !(Number.length ==10)  ){
+		// 		alert("Please enter a valid number ex. 0799855850");
+		// 		return false;
+		// 	}
 			
-			for (let i = 0; i < Number.length; i++) {
-				if (iChars.indexOf(Number.charAt(i)) != -1) {
-				alert ("The Number has special characters. \nThese are not allowed.\n");
-				return false;
-					}
-				}
-		return true;
-			}   
+		// 	for (let i = 0; i < Number.length; i++) {
+		// 		if (iChars.indexOf(Number.charAt(i)) != -1) {
+		// 		alert ("The Number has special characters. \nThese are not allowed.\n");
+		// 		return false;
+		// 			}
+		// 		}
+		// return true;
+		// 	}   
 
 
 
